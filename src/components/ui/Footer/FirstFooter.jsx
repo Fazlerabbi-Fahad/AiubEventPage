5a3192ee6694ab095cd49298201f2c8ecd3144af
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2024-02-14 04:26:25 +0600

    Footer Created

--- src/components/ui/Footer/FirstFooter.jsx
+++ src/components/ui/Footer/FirstFooter.jsx
@@ -7,9 +7,9 @@ import { TfiYoutube } from "react-icons/tfi";
 
 export default function FirstFooter() {
   return (
-    <div className="w-full h-[35px] bg-[#202020] mt-2">
-      <div className="max-w-screen-lg m-auto">
-        <div className="flex justify-between">
+    <div className="items-center p-1 bg-[#202020]">
+      <footer className="footer  max-w-screen-lg m-auto">
+        <aside className="grid-flow-col">
           <div>
             <div>
               <p className="text-[#646260] text-[5.59px] inline mr-1">
@@ -31,6 +31,8 @@ export default function FirstFooter() {
               </p>
             </div>
           </div>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end md:items-center">
           <div className="flex justify-evenly text-white py-2">
             <FaFacebookF className="mr-2" />
             <FaLinkedinIn className="mr-2" />
@@ -38,8 +40,43 @@ export default function FirstFooter() {
             <FaInstagram className="mr-2" />
             <TfiYoutube className="mr-2" />
           </div>
-        </div>
-      </div>
+        </nav>
+      </footer>
     </div>
+
+    // <div className="w-full h-[35px] bg-[#202020] mt-2">
+    //   <div className="max-w-screen-lg m-auto">
+    //     <div className="flex justify-between">
+    //       <div>
+    //         <div>
+    //           <p className="text-[#646260] text-[5.59px] inline mr-1">
+    //             2000-2019 ©
+    //           </p>
+    //           <p className="text-white text-[5.59px] inline mr-1">
+    //             AMERICAN INTERNATIONAL UNIVERSITY-BANGLADESH
+    //           </p>
+    //           <p className="text-[#646260] text-[5.59px] inline">
+    //             UNDER REGISTRATION 9988-COPR
+    //           </p>
+    //         </div>
+    //         <div className="mt-[-10px]">
+    //           <p className="text-[#646260] text-[5.59px] inline mr-1">
+    //             POWERED BY
+    //           </p>
+    //           <p className="text-white text-[5.59px] inline mr-1">
+    //             AIUB SOFTWARE DEVELOPMENT DEPARTMENT
+    //           </p>
+    //         </div>
+    //       </div>
+    //       <div className="flex justify-evenly text-white py-2">
+    //         <FaFacebookF className="mr-2" />
+    //         <FaLinkedinIn className="mr-2" />
+    //         <FaTwitter className="mr-2" />
+    //         <FaInstagram className="mr-2" />
+    //         <TfiYoutube className="mr-2" />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
--- src/components/ui/Footer/SecondFooter.jsx
+++ src/components/ui/Footer/SecondFooter.jsx
@@ -5,71 +5,67 @@ import { MdEmail } from "react-icons/md";
 
 export default function SecondFooter() {
   return (
-    <div className="w-full bg-[#181818] h-[173.51px]">
-      <div className="max-w-screen-lg m-auto py-5">
-        <div className="flex justify-evenly">
+    <div className="bg-[#181818]">
+      <footer className="footer py-10 bg-[#181818] max-w-screen-lg m-auto">
+        <aside>
           <div>
             <div>
               <img src={logo} width="58.31px" height="58.31px" />
             </div>
             <div>
-              <h4 className="text-[#ABABAB] text-[9px] font-bold mt-1">
+              <h4 className="text-[#ABABAB] text-md font-bold mt-1">
                 American International <br />
                 University-Bangladesh (AIUB)
               </h4>
-              <p className="text-[#ABABAB] text-[8px] flex justify-start mt-1">
-                <MdLocationOn className="mr-1" />
+              <p className="text-[#ABABAB] text-xs flex justify-start mt-2">
+                <MdLocationOn className="mr-1 mt-[3px]" />
                 408/1 (Old KA 66/1), Kuratoli, <br />
                 Khilkhet,Dhaka 1229, Bangladesh
               </p>
-              <p className="text-[#ABABAB] text-[8px] flex justify-start mt-1">
-                <MdEmail className="mr-1" />
+              <p className="text-[#ABABAB] text-xs flex justify-start mt-1">
+                <MdEmail className="mr-1 mt-[3px]" />
                 info@aiub.edu
               </p>
             </div>
           </div>
-          <div>
-            <p className="text-white text-[8.53px]">Become AIUBian</p>
-            <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
-            <p className="text-[#ABABAB] text-[8.53px]">Future Students</p>
-            <p className="text-[#ABABAB] text-[8.53px]">On Campus</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Admission</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Tuition Fees</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Scholarships</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Apply Now</p>
-          </div>
-          <div>
-            <p className="text-white text-[8.53px]">Academics</p>
-            <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
-            <p className="text-[#ABABAB] text-[8.53px]">Academic Calender</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Academic Regulations</p>
-            <p className="text-[#ABABAB] text-[8.53px]">
-              Faculty of Arts & Social Sciences
-            </p>
-            <p className="text-[#ABABAB] text-[8.53px]">
-              Faculty of Business Administration
-            </p>
-            <p className="text-[#ABABAB] text-[8.53px]">
-              Faculty of Engineering
-            </p>
-            <p className="text-[#ABABAB] text-[8.53px]">
-              Faculty of Science & Technology
-            </p>
-          </div>
-          <div>
-            <p className="text-white text-[8.53px]">Research</p>
-            <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
-            <p className="text-[#ABABAB] text-[8.53px]">AJBE</p>
-            <p className="text-[#ABABAB] text-[8.53px]">AJSE</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Past Conferences</p>
-            <p className="text-[#ABABAB] text-[8.53px]">Upcoming Conferences</p>
-            <p className="text-[#ABABAB] text-[8.53px]">
-              Collaborating Institues
-            </p>
-            <p className="text-[#ABABAB] text-[8.53px]">Innovations</p>
-          </div>
-        </div>
-      </div>
+        </aside>
+        <nav>
+          <p className="text-white text-md">Become AIUBian</p>
+          <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
+          <p className="text-[#ABABAB] text-sm">Future Students</p>
+          <p className="text-[#ABABAB] text-sm">On Campus</p>
+          <p className="text-[#ABABAB] text-sm">Admission</p>
+          <p className="text-[#ABABAB] text-sm">Tuition Fees</p>
+          <p className="text-[#ABABAB] text-sm">Scholarships</p>
+          <p className="text-[#ABABAB] text-sm">Apply Now</p>
+        </nav>
+        <nav>
+          <p className="text-white text-md">Academics</p>
+          <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
+          <p className="text-[#ABABAB] text-sm">Academic Calender</p>
+          <p className="text-[#ABABAB] text-sm">Academic Regulations</p>
+          <p className="text-[#ABABAB] text-sm">
+            Faculty of Arts & Social Sciences
+          </p>
+          <p className="text-[#ABABAB] text-sm">
+            Faculty of Business Administration
+          </p>
+          <p className="text-[#ABABAB] text-sm">Faculty of Engineering</p>
+          <p className="text-[#ABABAB] text-sm">
+            Faculty of Science & Technology
+          </p>
+        </nav>
+        <nav>
+          <p className="text-white text-md">Research</p>
+          <div className="divider w-[144.36px] divider-neutral p-0 m-0"></div>
+          <p className="text-[#ABABAB] text-sm">AJBE</p>
+          <p className="text-[#ABABAB] text-sm">AJSE</p>
+          <p className="text-[#ABABAB] text-sm">Past Conferences</p>
+          <p className="text-[#ABABAB] text-sm">Upcoming Conferences</p>
+          <p className="text-[#ABABAB] text-sm">Collaborating Institues</p>
+          <p className="text-[#ABABAB] text-sm">Innovations</p>
+        </nav>
+      </footer>
     </div>
   );
 }
